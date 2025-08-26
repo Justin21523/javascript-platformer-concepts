28e79b94a6e7530cd501fb9179cac50557a01179
Justin21523 <b0979865617@gmail.com>
2025-08-26 20:05:10 +0800

    chore(assets): add original assets files.

---
+++ assets/levels/PlatformSet.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="PlatformSet" tilewidth="129" tileheight="129" tilecount="18" columns="0">
+ <editorsettings>
+  <export target="PlatformSet.json" format="json"/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="18">
+  <image source="../freetileset/png/Tiles/1.png" width="128" height="128"/>
+ </tile>
+ <tile id="19">
+  <image source="../freetileset/png/Tiles/2.png" width="128" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="../freetileset/png/Tiles/3.png" width="128" height="128"/>
+ </tile>
+ <tile id="21">
+  <image source="../freetileset/png/Tiles/4.png" width="129" height="129"/>
+ </tile>
+ <tile id="22">
+  <image source="../freetileset/png/Tiles/5.png" width="128" height="128"/>
+ </tile>
+ <tile id="23">
+  <image source="../freetileset/png/Tiles/6.png" width="128" height="128"/>
+ </tile>
+ <tile id="24">
+  <image source="../freetileset/png/Tiles/7.png" width="128" height="128"/>
+ </tile>
+ <tile id="25">
+  <image source="../freetileset/png/Tiles/8.png" width="128" height="128"/>
+ </tile>
+ <tile id="26">
+  <image source="../freetileset/png/Tiles/9.png" width="128" height="128"/>
+ </tile>
+ <tile id="27">
+  <image source="../freetileset/png/Tiles/10.png" width="128" height="128"/>
+ </tile>
+ <tile id="28">
+  <image source="../freetileset/png/Tiles/11.png" width="128" height="128"/>
+ </tile>
+ <tile id="29">
+  <image source="../freetileset/png/Tiles/12.png" width="128" height="128"/>
+ </tile>
+ <tile id="30">
+  <image source="../freetileset/png/Tiles/13.png" width="128" height="93"/>
+ </tile>
+ <tile id="31">
+  <image source="../freetileset/png/Tiles/14.png" width="128" height="93"/>
+ </tile>
+ <tile id="32">
+  <image source="../freetileset/png/Tiles/15.png" width="128" height="93"/>
+ </tile>
+ <tile id="33">
+  <image source="../freetileset/png/Tiles/16.png" width="128" height="128"/>
+ </tile>
+ <tile id="34">
+  <image source="../freetileset/png/Tiles/17.png" width="128" height="99"/>
+ </tile>
+ <tile id="35">
+  <image source="../freetileset/png/Tiles/18.png" width="128" height="128"/>
+ </tile>
+</tileset>
