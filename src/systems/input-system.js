857bd5f97d6da4e265acf26b7764064cc7a5f82e
Justin21523 <b0979865617@gmail.com>
2025-09-01 20:38:38 +0800

    fix(input): correct component imports and system implementation

--- src/systems/input-system.js
+++ src/systems/input-system.js
@@ -1,16 +1,26 @@
 // src/systems/input-system.js
 import { snapshot } from "../input.js";
-import { COMPONENT_TYPES, components } from "../ecs/components.js";
+import { ComponentBits } from "../ecs/components.js";
 
 export class InputSystem {
+  constructor() {
+    this.world = null;
+  }
+
+  setWorld(world) {
+    this.world = world;
+  }
+
   update(dt) {
+    if (!this.world) return;
+
     const inputSnapshot = snapshot();
 
     // 找到所有具有 Input component 的實體
-    const entities = this.world.query(COMPONENT_TYPES.INPUT);
+    const entities = this.world.query(["Input"]);
 
     for (const entityId of entities) {
-      const input = components.Input[entityId];
+      const input = this.world.getComponent(entityId, "Input");
       if (input) {
         // 更新輸入狀態
         Object.assign(input, inputSnapshot);
