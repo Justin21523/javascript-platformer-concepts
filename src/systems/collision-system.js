e1f8c4de59510d93ddf05805b8562439c81aa173
Justin21523 <b0979865617@gmail.com>
2025-09-01 20:00:32 +0800

    feat(collision): implement per-axis AABB collision resolution system

--- src/systems/collision-system.js
+++ src/systems/collision-system.js
@@ -1,11 +1,12 @@
 // src/systems/collision-system.js
 import { aabbOverlap } from "../core/math.js";
-import { PHYSICS } from "../config.js";
+import { COLLISION } from "../config.js";
 
 export class CollisionSystem {
   constructor(world, tileMap) {
     this.world = world;
     this.tileMap = tileMap;
+    // 碰撞旗標儲存
     this.flags = {
       onGround: new Map(),
       hitWall: new Map(),
@@ -36,18 +37,18 @@ export class CollisionSystem {
     const velocity = this.world.getComponent(entity, "Velocity");
     const aabb = this.world.getComponent(entity, "AABB");
 
-    // X 軸碰撞解算
+    // X 軸碰撞解算 (先處理水平移動)
     const nextX = transform.x + velocity.vx * dt;
-    if (this.checkXCollision(entity, nextX, transform.y, aabb)) {
-      velocity.vx = 0;
+    if (this.checkXCollision(entity, nextX, transform.y, aabb, velocity)) {
       this.flags.hitWall.set(entity, true);
     } else {
       transform.x = nextX;
     }
 
-    // Y 軸碰撞解算
+    // Y 軸碰撞解算 (再處理垂直移動)
     const nextY = transform.y + velocity.vy * dt;
-    if (this.checkYCollision(entity, transform.x, nextY, aabb)) {
+    if (this.checkYCollision(entity, transform.x, nextY, aabb, velocity)) {
+      // 判斷碰撞方向
       if (velocity.vy > 0) {
         // 向下碰撞 = 落地
         this.flags.onGround.set(entity, true);
@@ -55,9 +56,9 @@ export class CollisionSystem {
         // 向上碰撞 = 撞天花板
         this.flags.hitCeil.set(entity, true);
       }
-      velocity.vy = 0;
     } else {
       transform.y = nextY;
+      // 沒有 Y 軸碰撞表示不在地面
       this.flags.onGround.set(entity, false);
     }
   }
@@ -83,19 +84,21 @@ export class CollisionSystem {
           tile.h
         )
       ) {
-        // MTV 修正 (簡化版)
-        const centerX = x + aabb.ox + aabb.w / 2;
+        // 計算最小平移向量 (MTV) 進行位置修正
+        const transform = this.world.getComponent(entity, "Transform");
+        const entityCenterX = x + aabb.ox + aabb.w / 2;
         const tileCenterX = tile.x + tile.w / 2;
 
-        if (centerX < tileCenterX) {
+        if (entityCenterX < tileCenterX) {
           // 撞到右牆，推回左邊
-          const transform = this.world.getComponent(entity, "Transform");
-          transform.x = tile.x - aabb.w - aabb.ox - 1;
+          transform.x = tile.x - aabb.w - aabb.ox - COLLISION.EPSILON;
         } else {
           // 撞到左牆，推回右邊
-          const transform = this.world.getComponent(entity, "Transform");
-          transform.x = tile.x + tile.w - aabb.ox + 1;
+          transform.x = tile.x + tile.w - aabb.ox + COLLISION.EPSILON;
         }
+
+        // 停止水平速度
+        velocity.vx = 0;
         return true;
       }
     }
@@ -123,19 +126,21 @@ export class CollisionSystem {
           tile.h
         )
       ) {
-        // MTV 修正
-        const centerY = y + aabb.oy + aabb.h / 2;
+        // 計算 MTV 進行位置修正
+        const transform = this.world.getComponent(entity, "Transform");
+        const entityCenterY = y + aabb.oy + aabb.h / 2;
         const tileCenterY = tile.y + tile.h / 2;
 
-        if (centerY < tileCenterY) {
+        if (entityCenterY < tileCenterY) {
           // 撞到地面，推到上方
-          const transform = this.world.getComponent(entity, "Transform");
-          transform.y = tile.y - aabb.h - aabb.oy - 1;
+          transform.y = tile.y - aabb.h - aabb.oy - COLLISION.EPSILON;
         } else {
           // 撞到天花板，推到下方
-          const transform = this.world.getComponent(entity, "Transform");
-          transform.y = tile.y + tile.h - aabb.oy + 1;
+          transform.y = tile.y + tile.h - aabb.oy + COLLISION.EPSILON;
         }
+
+        // 停止垂直速度
+        velocity.vy = 0;
         return true;
       }
     }
