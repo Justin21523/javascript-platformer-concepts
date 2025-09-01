fbe5298e0a3627eebcd545715604707776389bb8
Justin21523 <b0979865617@gmail.com>
2025-09-01 19:36:56 +0800

    feat(collision): implement per-axis AABB collision resolution system

---
+++ src/systems/collision-system.js
@@ -0,0 +1,155 @@
+// src/systems/collision-system.js
+import { aabbOverlap } from "../core/math.js";
+import { PHYSICS } from "../config.js";
+
+export class CollisionSystem {
+  constructor(world, tileMap) {
+    this.world = world;
+    this.tileMap = tileMap;
+    this.flags = {
+      onGround: new Map(),
+      hitWall: new Map(),
+      hitCeil: new Map(),
+    };
+  }
+
+  update(dt) {
+    // 清除上一幀旗標
+    this.flags.onGround.clear();
+    this.flags.hitWall.clear();
+    this.flags.hitCeil.clear();
+
+    const entities = this.world.query([
+      "Transform",
+      "Velocity",
+      "AABB",
+      "Collider",
+    ]);
+
+    for (const entity of entities) {
+      this.resolveCollisions(entity, dt);
+    }
+  }
+
+  resolveCollisions(entity, dt) {
+    const transform = this.world.getComponent(entity, "Transform");
+    const velocity = this.world.getComponent(entity, "Velocity");
+    const aabb = this.world.getComponent(entity, "AABB");
+
+    // X 軸碰撞解算
+    const nextX = transform.x + velocity.vx * dt;
+    if (this.checkXCollision(entity, nextX, transform.y, aabb)) {
+      velocity.vx = 0;
+      this.flags.hitWall.set(entity, true);
+    } else {
+      transform.x = nextX;
+    }
+
+    // Y 軸碰撞解算
+    const nextY = transform.y + velocity.vy * dt;
+    if (this.checkYCollision(entity, transform.x, nextY, aabb)) {
+      if (velocity.vy > 0) {
+        // 向下碰撞 = 落地
+        this.flags.onGround.set(entity, true);
+      } else {
+        // 向上碰撞 = 撞天花板
+        this.flags.hitCeil.set(entity, true);
+      }
+      velocity.vy = 0;
+    } else {
+      transform.y = nextY;
+      this.flags.onGround.set(entity, false);
+    }
+  }
+
+  checkXCollision(entity, x, y, aabb) {
+    const tiles = this.tileMap.getTilesInAABB(
+      x + aabb.ox,
+      y + aabb.oy,
+      aabb.w,
+      aabb.h
+    );
+
+    for (const tile of tiles) {
+      if (
+        aabbOverlap(
+          x + aabb.ox,
+          y + aabb.oy,
+          aabb.w,
+          aabb.h,
+          tile.x,
+          tile.y,
+          tile.w,
+          tile.h
+        )
+      ) {
+        // MTV 修正 (簡化版)
+        const centerX = x + aabb.ox + aabb.w / 2;
+        const tileCenterX = tile.x + tile.w / 2;
+
+        if (centerX < tileCenterX) {
+          // 撞到右牆，推回左邊
+          const transform = this.world.getComponent(entity, "Transform");
+          transform.x = tile.x - aabb.w - aabb.ox - 1;
+        } else {
+          // 撞到左牆，推回右邊
+          const transform = this.world.getComponent(entity, "Transform");
+          transform.x = tile.x + tile.w - aabb.ox + 1;
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkYCollision(entity, x, y, aabb) {
+    const tiles = this.tileMap.getTilesInAABB(
+      x + aabb.ox,
+      y + aabb.oy,
+      aabb.w,
+      aabb.h
+    );
+
+    for (const tile of tiles) {
+      if (
+        aabbOverlap(
+          x + aabb.ox,
+          y + aabb.oy,
+          aabb.w,
+          aabb.h,
+          tile.x,
+          tile.y,
+          tile.w,
+          tile.h
+        )
+      ) {
+        // MTV 修正
+        const centerY = y + aabb.oy + aabb.h / 2;
+        const tileCenterY = tile.y + tile.h / 2;
+
+        if (centerY < tileCenterY) {
+          // 撞到地面，推到上方
+          const transform = this.world.getComponent(entity, "Transform");
+          transform.y = tile.y - aabb.h - aabb.oy - 1;
+        } else {
+          // 撞到天花板，推到下方
+          const transform = this.world.getComponent(entity, "Transform");
+          transform.y = tile.y + tile.h - aabb.oy + 1;
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // 旗標查詢 API
+  isOnGround(entity) {
+    return this.flags.onGround.get(entity) || false;
+  }
+  hasHitWall(entity) {
+    return this.flags.hitWall.get(entity) || false;
+  }
+  hasHitCeil(entity) {
+    return this.flags.hitCeil.get(entity) || false;
+  }
+}
